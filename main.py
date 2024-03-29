8109b84bc8205e87d2189a4f9576c35bdef2402c
Xhexos822 <150151083+Xhexos822@users.noreply.github.com>
2024-03-29 13:08:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import uuid
 
 app = Flask(__name__)
+CORS(app)
 
 # Load keys and values from JSON file
 with open('keys.json', 'r') as file:
