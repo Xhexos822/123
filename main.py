eda14ce15e2d34da1aca2d1c11b17c6fa7ea09d4
Xhexos822 <150151083+Xhexos822@users.noreply.github.com>
2024-04-04 08:45:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,19 @@ with open('keys.json') as f:
 
 
 # Endpoint for checking key
-@app.route('/check_key', methods=['POST', 'GET'])
+@app.route('/check_key', methods=['POST'])
 def check_key():
-    if request.method == 'POST':
-        data = request.json
-        key = data.get('key')
+    data = request.json
+    key = data.get('key')
 
-        if not key:
-            return jsonify({'error': 'Key is required'}), 400
+    if not key:
+        return jsonify({'error': 'Key is required'}), 400
 
-        if key in keys:
-            # Return the validity of the key along with the associated username
-            return jsonify({'valid': True, 'username': keys[key]['user']}), 200
-        else:
-            return jsonify({'valid': False}), 200
+    if key in keys:
+        # Return the validity of the key along with the associated username
+        return jsonify({'valid': True, 'username': keys[key]['user']}), 200
     else:
-        return jsonify({'error': 'Method not allowed'}), 405
+        return jsonify({'valid': False}), 200
 
 
 if __name__ == '__main__':
