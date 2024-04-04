b2ee4e2f134769e9155043a319d38b6dc570312f
Xhexos822 <150151083+Xhexos822@users.noreply.github.com>
2024-04-04 06:40:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import json
 
 app = Flask(__name__)
 
-# Load keys from JSON file
+# Load keys
 with open('keys.json') as f:
     keys = json.load(f)
 
 
-# Endpoint to check key
+# Endpoint for checking key
 @app.route('/check_key', methods=['POST', 'GET'])
 def check_key():
     data = request.json
@@ -18,7 +18,8 @@ def check_key():
         return jsonify({'error': 'Key is required'}), 400
 
     if key in keys:
-        return jsonify({'valid': True}), 200
+        # Return the validity of the key along with the associated username
+        return jsonify({'valid': True, 'username': keys[key]['user']}), 200
     else:
         return jsonify({'valid': False}), 200
 
